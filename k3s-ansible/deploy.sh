f43dd6eef01b29a4ff450d63aa138201bc5395ee
ranygo12-pixel <ranygo12@gmail.com>
2026-03-02 22:29:56 +0900

    Update deploy.sh

--- k3s-ansible/deploy.sh
+++ k3s-ansible/deploy.sh
@@ -41,7 +41,7 @@ ansible all -i $INV -m ping
 
 # 4. 전체 플레이북 실행
 echo "📦 K3s 전체 설치 프로세스 가동 (site.yml)..."
-ansible-playbook -i $INV site.yml -vvvv
+ansible-playbook -i $INV site.yml -e "@group_vars/all.yml" -v
 
 echo ""
 echo "✅ K3s 클러스터 배포가 성공적으로 완료되었습니다!"
