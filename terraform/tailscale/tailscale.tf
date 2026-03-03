b13215d4f36f99c081f9f0ab17d0fc90cc537eda
ranygo12-pixel <ranygo12@gmail.com>
2026-03-03 17:55:41 +0900

    Update Tailscale ACL configuration for dynamic IDs

--- terraform/tailscale/tailscale.tf
+++ terraform/tailscale/tailscale.tf
@@ -1,15 +1,21 @@
+# terraform/tailscale/tailscale.tf 예시
+
+data "aws_instances" "k3s_nodes" {
+  filter {
+    name   = "tag:Name"
+    values = ["k3s-*"] # AWS 인프라에서 설정한 태그 이름에 맞게 수정
+  }
+}
+
 resource "tailscale_acl" "main_acl" {
   acl = jsonencode({
-    # 태그 소유권 설정 (GHA와 서버에 부여할 권한)
-    tagOwners = {
-      "tag:k3s" = ["group:admin"]
-    },
-    # 접근 제어 규칙: k3s 태그 노드끼리, 그리고 GHA(tag:k3s)와의 통신 허용
+    # ... 생략 ...
     acls = [
       {
-        "action" = "accept",
-        "src"    = ["tag:k3s"],
-        "dst"    = ["tag:k3s:*"]
+        action = "accept",
+        src    = ["tag:k3s"],
+        # 이제 (known after apply)가 아닌 실시간 ID가 들어갑니다.
+        dst    = [for id in data.aws_instances.k3s_nodes.ids : "devices:${id}:*"]
       }
     ]
   })
